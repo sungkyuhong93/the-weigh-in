156d2f24bc2027cf22dcf5942a655c7c1d58f6e9
Sung Kyu Hong <sungkyuhong93@gmail.com>
2020-01-23 01:10:13 -0500

    test

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,7 +1,33 @@
 import React from "react"
+import { graphql, useStaticQuery } from "gatsby"
+
+import Layout from "../components/layout"
+import HomeHero from "../components/HomeHero"
+
+const getAbout = graphql`
+query{
+  about:contentfulAbout {
+   aboutHero{
+      fluid {
+        ...GatsbyContentfulFluid
+      }
+    }
+    aboutText
+  }
+}
+`
 
 const About = () => {
-  return <div>hi from about page</div>
+  const { about } = useStaticQuery(getAbout)
+  const { aboutHero, aboutText } = about
+  return (
+    <Layout>
+      <HomeHero heroImg={aboutHero} heroText={'About'} />
+      <div className="blog-item-div">
+        <p className="blog-item-description">{aboutText}</p>
+      </div>
+    </Layout>
+  )
 }
 
 export default About
--- src/templates/blog-template.js
+++ src/templates/blog-template.js
@@ -1,10 +1,9 @@
 import React from "react"
 import { graphql } from "gatsby"
-import Img from "gatsby-image"
 
 import Layout from "../components/layout"
 import HomeHero from "../components/HomeHero"
-import Hamburger from "../components/Hamburger"
+
 
 const BlogTemplate = ({ data }) => {
   const {
