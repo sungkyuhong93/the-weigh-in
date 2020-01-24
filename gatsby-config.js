e61b67ebceff873c072f5318b5385bdb1067f36f
Sung Kyu Hong <sungkyuhong93@gmail.com>
2020-01-23 23:42:13 -0500

    changed title

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,8 +3,8 @@ require("dotenv").config({
 })
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
+    title: `The Weigh In`,
+    description: `The Weigh In`,
     author: `@gatsbyjs`,
   },
   plugins: [
